e4b2766a0b5469683f12865af832dfc655890708
kundanaSakhamuri98 <s545248@nwmissouri.edu>
2021-11-03 00:00:47 -0500

    added kid as  an endpoint

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var kidRouter = require('./routes/kid');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/kid', kidRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
