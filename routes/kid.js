5776b89095863570a28c878ac5587d7e62d99b27
kundanaSakhamuri98 <s545248@nwmissouri.edu>
2021-11-03 14:34:39 -0500

    added kid.pug to kid.js

--- routes/kid.js
+++ routes/kid.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Search Result for Kid' });
+  res.render('kid', { title: 'Search Result for Kid' });
 });
 
 module.exports = router;
